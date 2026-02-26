3f87c3641b70620b55b5a3a7e7a1de7cdfa1a008
yaftrie <yaftrie@gmail.com>
2026-02-26 21:00:44 +0100

    create logs in launcher

--- launcher/launch.js
+++ launcher/launch.js
@@ -1,25 +1,52 @@
 const { Launch } = require('minecraft-java-core');
 const path = require('path');
+const fs = require('fs');
 
-// Usage: launch.js [path] [versionId] [username] [minMem] [maxMem]
+// Usage: launch.js [log_path] [mc_path] [versionId] [username] [minMem] [maxMem]
 args = process.argv.slice(2);
 
-const mc_path = args[0] || path.join(__dirname, 'minecraft');
-const versionId = args[1] || "1.0";
-const username = args[2] || "default";
-const minMem = args[3] || "1G";
-const maxMem = args[4] || "2G";
+const logPath = args[0] || path.join(__dirname, 'app.log');
+const mc_path = args[1] || path.join(__dirname, 'minecraft');
+const versionId = args[2] || "1.0";
+const username = args[3] || "default";
+const minMem = args[4] || "1G";
+const maxMem = args[5] || "2G";
+
+// Function to write logs to file
+function writeLog(message) {
+  fs.appendFile(logPath, message, (err) => {
+    if (err) throw err;
+  });
+}
 
 // create launcher instance
 const launcher = new Launch();
 
-// add event listeners
+let lastSent = -1;
+
 launcher.on('progress', (progress, size, element) => {
-  console.log(`${element} -> ${(progress/size*100).toFixed(2)}%`);
+  const percent = Math.floor((progress / size) * 100);
+
+  // bloque actual (0,10,20,...100)
+  const currentBlock = Math.floor(percent / 10) * 10;
+
+  if (currentBlock !== lastSent && currentBlock % 10 === 0) {
+    lastSent = currentBlock;
+
+    if (process.send) {
+      writeLog(`Progress: ${currentBlock}% - ${element}\n`);
+      process.send({
+        type: 'progress',
+        percent: currentBlock,
+        element
+      });
+    }
+  }
 });
-launcher.on('data', console.log);
-launcher.on('error', console.error);
-launcher.on('close', () => console.log('Descarga/Inicio terminado'));
+
+launcher.on('data', (data) => writeLog(data));
+launcher.on('error', (data) => writeLog(`Error: ${data}`));
+launcher.on('close', () => writeLog(`Launch Closed at ${new Date().toISOString()}`));
 
 // Define options
 const options = {
